d0bcf57454a362d8c2fc611ffaba5bb4774c2d4e
sarp_03 <sarpereroglu2@gmail.com>
2026-03-26 20:14:08 +0100

    New windows & first UI changes

---
+++ Views/new_connection.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Quack.sh;
+
+public partial class new_connection : Window
+{
+    public new_connection()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Views/preferences.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Quack.sh;
+
+public partial class preferences : Window
+{
+    public preferences()
+    {
+        InitializeComponent();
+    }
+}
